43b94894a3db1ed9760e82fb11f8fd18d9d9c8ac
Vladislav Rusakov <vladisrusakov@yandex.ru>
2022-06-17 00:47:21 +0300

    Add supporting classes for jwt authorization

---
+++ src/main/java/ru/pycak/todolistapp/security/jwt/filter/JwtTokenFilter.java
@@ -0,0 +1,6 @@
+package ru.pycak.todolistapp.security.jwt.filter;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+public abstract class JwtTokenFilter extends OncePerRequestFilter {
+}
---
+++ src/main/java/ru/pycak/todolistapp/security/jwt/provider/JwtAuthenticationProvider.java
@@ -0,0 +1,17 @@
+package ru.pycak.todolistapp.security.jwt.provider;
+
+import org.springframework.security.core.Authentication;
+import ru.pycak.todolistapp.security.jwt.exception.JwtAuthenticationException;
+
+import javax.servlet.http.HttpServletRequest;
+
+public interface JwtAuthenticationProvider {
+
+    /**
+     * @param request http request which contains Authorization header with Bearer token
+     * @return Authentication token
+     * @throws JwtAuthenticationException if request Authorization header is absent,
+     * or the Bearer JWT token is expired or invalid
+     */
+    Authentication getAuthentication(HttpServletRequest request) throws JwtAuthenticationException;
+}
